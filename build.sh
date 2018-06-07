43ec9dd838e7355e2a77783facaea4d13d42a0f7
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2018-06-07 12:40:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -76,7 +76,8 @@ buildDockerImage () {
     echo -e "\nWriting version ${DOCKER_TAG} to files"
     sed -i.org "s/__APP_VERSION__/${DOCKER_TAG}/g" ${BUILD_DIR}/site/*.html
     rm -f ${BUILD_DIR}/site/*.org
-
+    
+    ls
     echo -e "\nBuilding Docker image"
     docker build -t ${DOCKER_REG_HUB}/${DOCKER_REPO}:${DOCKER_TAG} ${BUILD_DIR} || errorExit "Building ${DOCKER_REPO}:${DOCKER_TAG} failed"
 }
